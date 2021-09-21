da060c52100bf0ba12bc45f5ad28a01e839ae7d8
Roberto Guineo Avendaño <rguineoa@gmail.com>
2021-09-21 20:09:08 -0300

    actualizar codigo

--- js/main.js
+++ js/main.js
@@ -13,43 +13,109 @@
 // alert("Hola " + nombre + " " + apellido + ", bienvenido a nuestro sitio web")
 // console.log("Hola " + nombre + " " + apellido + ", bienvenido a nuestro sitio web")
 
-var num1 = prompt("Ingrese el 1° numero", "")
-var num2 = prompt("Ingrese el 2° numero", "")
+var num1 = prompt("Ingrese el lado de un cuadrado", "")
+// var num2 = prompt("Ingrese el 2° numero", "")
+
+document.write("El area del cuadrado es: " + area_cuadrado(parseInt(num1)) + "<br>")
+document.write("El perimetro del cuadrado es: " + perimetro_cuadrado(parseInt(num1)) + "<br>")
+
+
+
+
+function area_cuadrado(a){
+	return a*a
+}
+
+function perimetro_cuadrado(a){
+	return 4*a
+}
+
+function area_rectangulo(a){
+	return a*b
+}
+
+function perimetro_rectangulo(a){
+	return 2*a+2*b
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var resultado = parseInt(num1) + parseInt(num2)
 // document.write("El resultado es: " + (parseInt(num1)+parseInt(num2)))
 
-sumar(parseInt(num1), parseInt(num2));
-multiplicar(parseInt(num1), parseInt(num2));
-restar(parseInt(num1), parseInt(num2));
-dividir(parseInt(num1), parseInt(num2));
-resto(parseInt(num1), parseInt(num2));
+// sumar(parseInt(num1), parseInt(num2));
+// multiplicar(parseInt(num1), parseInt(num2));
+// restar(parseInt(num1), parseInt(num2));
+// dividir(parseInt(num1), parseInt(num2));
+// resto(parseInt(num1), parseInt(num2));
 
-function sumar(a, b){
-	document.write("El resultado de la suma es: " + (a + b) + "<br>")
-}
+// document.write("resultado: " + (sumar(parseInt(num1), parseInt(num2)) * dividir(parseInt(num1), parseInt(num2))))
 
-function restar(a, b){
-	document.write("El resultado de la resta es: " + (a - b) + "<br>")
-}
 
-function multiplicar(a, b){
-	document.write("El resultado de la multiplicacion es: " + (a * b) + "<br>")
-}
+// // function sumar(a, b){
+// // 	document.write("El resultado de la suma es: " + (a + b) + "<br>")
+// // }
 
-function dividir(a, b){
+// function sumar(a, b){
+// 	return a+b
+// }
 
-	if (b != 0){
-		document.write("El resultado de la division es: " + (a / b).toFixed(3) + "<br>")
-	} else {
-		document.write("El resultado de la division es: ERROR DIVISION POR 0 <br>")
-	}
-}
+// function restar(a, b){
+// 	document.write("El resultado de la resta es: " + (a - b) + "<br>")
+// }
 
-function resto(a, b){
-	if (b != 0){
-		document.write("El resultado del resto de la division es: " + (a % b) + "<br>")
-	} else {
-		document.write("El resultado de la division es: ERROR DIVISION POR 0 <br>")
-	}
-}
+// function multiplicar(a, b){
+// 	document.write("El resultado de la multiplicacion es: " + (a * b) + "<br>")
+// }
+
+// // function dividir(a, b){
+// // 	if (b != 0){
+// // 		document.write("El resultado de la division es: " + (a / b).toFixed(3) + "<br>")
+// // 	} else {
+// // 		document.write("El resultado de la division es: ERROR DIVISION POR 0 <br>")
+// // 	}
+// // }
+
+// function dividir(a, b){
+// 	if (b != 0){
+// 		return (a / b).toFixed(3)
+// 	} else {
+// 		return 1
+// 	}
+// }
+
+// function resto(a, b){
+// 	if (b != 0){
+// 		document.write("El resultado del resto de la division es: " + (a % b) + "<br>")
+// 	} else {
+// 		document.write("El resultado de la division es: ERROR DIVISION POR 0 <br>")
+// 	}
+// }
